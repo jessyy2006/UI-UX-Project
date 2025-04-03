97e19a77d763a48f1b5ff708f216c55bb8fe36d5
Jessica Young <worldlive145@gmail.com>
2025-04-02 21:19:08 -0400

    new slider

--- iterations/script.js
+++ iterations/script.js
@@ -1,10 +1,2 @@
 
 // Slider
-$(function() {
-	var rangePercent = $('[type="range"]').val();
-	$('[type="range"]').on('change input', function() {
-		rangePercent = $('[type="range"]').val();
-		$('h4').text(rangePercent);
-		$('[type="range"]').css('filter', 'hue-rotate(-' + rangePercent + 'deg)');
-	});
-});
